e3f60adb1cab22985f668a38828bcc3d5b062080
Tanner Kothlow <tannerkothlow@msn.com>
2023-01-15 11:30:27 -0600

    user model

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: {
+                validator: function(v) {
+                    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+                },
+                message: props => `${props.value} is not a valid email!`
+            },
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+);
+
+userSchema
+    .virtual('friendCount')
+    .get(function () {
+        return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
+
